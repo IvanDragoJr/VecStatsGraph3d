3c39f5ab708e1a44fd906bcfa29dcea134cc682a
pedro <palfonsojimenez.external@epo.org>
2018-09-16 20:27:30 +0200

    Repackage module statistics manager

--- src/Test.py
+++ src/Test.py
@@ -3,12 +3,15 @@
 from src.graphs.DensityGraph import DensityGraph
 from src.manager.FileManager import FileManager
 from src.graphs.ModuleAngleGraph import ModuleAngleGraph
+from src.manager.ModuleStatisticsManager import ModuleStatisticsManager
 from src.util.ArithmeticUtil import ArithmeticUtil
+
 
 fileManager = FileManager()
 fileManager.load_data()
 dat = fileManager.loadData3D(fileManager.get_vectorsMatrix())
 
+moduleStatisticsManager = ModuleStatisticsManager()
 # DensityGraph.draw_density_graph(dat)
 # ModuleAngleGraph.draw_module_angle_distrib(dat)
-ArithmeticUtil.kurtois_module_coefficient(fileManager.getColumnAsArray(0, dat))
+moduleStatisticsManager.kurtois_module_coefficient(fileManager.getColumnAsArray(0, dat))
