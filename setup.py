a3ca451bc292f99bad7752f7b06b917995da8a92
pedro <palfonsojimenez.external@epo.org>
2018-10-18 20:08:47 +0200

    Add install requieres to setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     license='Apache 2.0',
     author='Pedro Alfonso',
     author_email='p.alfonso.jimenez@gmail.com',
-    description='Spherical statistcis analysis'
+    description='Spherical statistcis analysis', install_requires=['matplotlib', 'scipy', 'numpy']
 )
